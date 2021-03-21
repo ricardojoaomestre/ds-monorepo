d07e0c22f3e48c82b87403b4d3c3605aeadf5486
Ricardo Mestre <ricardojoao85@gmail.com>
2021-03-21 16:02:55 +0000

    add button component, storybook and emotion

--- packages/components/src/button/index.js
+++ packages/components/src/button/index.js
@@ -1,5 +1,19 @@
 import React from "react";
+import styled from "@emotion/styled";
 
-const Button = ({ children, ...rest }) => <button {...rest}>{children}</button>;
+const _ButtonBase = styled.button`
+  padding: 16px 8px;
+  font-weight: bold;
+  border: 1px solid transparent;
+  border-radius: 8px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Button = ({ children, ...rest }) => (
+  <_ButtonBase {...rest}>{children}</_ButtonBase>
+);
 
 export default Button;
